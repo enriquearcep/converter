73af86819da723d519e3c169f141b211111bf14e
Enrique <enriquearce00@gmail.com>
2023-11-01 17:36:18 -0600

    Fonts | Font name fix

--- Converter/MauiProgram.cs
+++ Converter/MauiProgram.cs
@@ -13,7 +13,7 @@ namespace Converter
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("fontello.ttf", "Icons");
+                    fonts.AddFont("Fontello.ttf", "Icons");
                 });
 
 #if DEBUG
